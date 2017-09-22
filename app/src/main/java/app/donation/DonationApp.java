0aff9aaa61af97c4b4c36bc3ea3c2c5aadf6a12f
Gaz1676 <gazfleming@gmail.com>
2017-09-22 16:54:07 +0100

    Imported CalligraphyConfig for context and set defaultFontPath to new simpsons.ttf file

--- app/src/main/java/app/donation/DonationApp.java
+++ app/src/main/java/app/donation/DonationApp.java
@@ -4,25 +4,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 import android.app.Application;
-import android.util.Log;
 import android.widget.Toast;
 
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
+
+// any CRUD done through this class
 public class DonationApp extends Application
 {
     public final int       target       = 10000;
     public int             totalDonated = 0;
-    public List <Donation> donations    = new ArrayList<>();
+    public List <Donation> donations    = new ArrayList<>(); // creates new arraylist
 
-    public boolean newDonation(Donation donation)
-    {
+    public boolean newDonation(Donation donation) {
         boolean targetAchieved = totalDonated > target;
         if (!targetAchieved)
         {
             donations.add(donation);
             totalDonated += donation.amount;
-        }
-        else
-        {
+        } else {
             Toast toast = Toast.makeText(this, "Target Exceeded!", Toast.LENGTH_SHORT);
             toast.show();
         }
@@ -30,9 +29,12 @@ public class DonationApp extends Application
     }
 
     @Override
-    public void onCreate()
-    {
+    public void onCreate() {
         super.onCreate();
-        Log.v("Donate", "Donation App Started");
+        CalligraphyConfig.initDefault(new CalligraphyConfig.Builder() // adds new simpson font
+                        .setDefaultFontPath("fonts/simpsons.ttf")
+                        .setFontAttrId(R.attr.fontPath)
+                        .build()
+        );
     }
 }
